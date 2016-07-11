9a263de35ebbb40cdddaab5102dd9607680bcebf
Code Hz <CodeHz@outlook.com>
2016-07-11 14:20:45 +0800

    init

---
+++ page.js
@@ -0,0 +1,29 @@
+const {remote}      = require('electron');
+const path          = require('path');
+const currentWindow = remote.getCurrentWindow();
+
+function setThumbnailToolbar()  {
+    currentWindow.setThumbarButtons([{
+        tooltip: 'rewind',
+        icon: path.join(__dirname, 'rewind.png')
+    }, {
+        tooltip: this.state === 'play' ? 'pause' : 'play',
+        icon: path.join(__dirname, 'play.png')
+    }, {
+        tooltip: 'forward',
+        icon: path.join(__dirname, 'forward.png')
+    }]);
+}
+
+function goFullScreen() {
+    currentWindow.setFullScreen(true);
+}
+
+function exitFullScreen() {
+    currentWindow.setFullScreen(false);
+}
+
+function hideAndShow() {
+    currentWindow.hide();
+    setTimeout(() => currentWindow.show(), 2000);
+}
---
+++ readme.md
@@ -0,0 +1,29 @@
+# e-test
+
+> My super-duper app
+
+
+## Dev
+
+```
+$ npm install
+```
+
+### Run
+
+```
+$ npm start
+```
+
+### Build
+
+```
+$ npm run build
+```
+
+Builds the app for OS X, Linux, and Windows, using [electron-packager](https://github.com/electron-userland/electron-packager).
+
+
+## License
+
+MIT © [Code Hz](http://codehz.one)
